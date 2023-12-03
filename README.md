70b548f580b2ec873368b2f5c6b2c47e9c979922
tonychg <tonychg7@pm.me>
2023-12-03 10:55:07 +0100

    feat(config): bootstrap project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Automatic backup
+
+Use restic to backup files and folders automatically on Scaleway Object Storage.
+The secrets are encrypted with sops and age.
---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Automatic backup using restic";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      with pkgs;
+      {
+        devShell = mkShell {
+          buildInputs = [
+            git
+            restic
+            go-task
+            sops
+            age
+          ];
+
+          shellHook = ''
+            task -l
+          '';
+        };
+      });
+}
